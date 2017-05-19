6a5b7962b4c3590eae933b363c9ba1e76d0bee58
Elton Cheng <elton.c22@gmail.com>
2017-05-19 14:07:02 -0500

    Updated csv and arff file for problem 1, since it had only 900 examples. Needed to have 1000

--- createData.py
+++ createData.py
@@ -6,14 +6,14 @@ import copy
 randomDict = {1:[0,0],2:[0,1],3:[1,0],4:[1,1]}
 
 data = []
-f = open('randTest2.csv','w')
-header = ','.join('Column ' + str(x) for x in range(0,102))
+f = open('randTest.csv','w')
+header = ','.join('Column ' + str(x) for x in range(0,900))
 f.write(header + ',ClassLabel\n')
 
-for i in range(0,900):
+for i in range(0,999):
     dictIndex = randint(1, 4)
     randArray = copy.deepcopy(randomDict[dictIndex])
-    for j in range(0,100):
+    for j in range(0,898):
         valIndex = randint(0, 255)
         randArray.append(valIndex)
     randArray.append(dictIndex)
